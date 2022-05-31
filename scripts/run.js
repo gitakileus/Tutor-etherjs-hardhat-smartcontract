b77074922eeabe524217af391db360515d402ba5
gitakileus <akileus0902@gmail.com>
2022-05-31 00:42:06 -0400

    complete

---
+++ scripts/run.js
@@ -0,0 +1,49 @@
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther('0.1'),
+  })
+  await waveContract.deployed()
+
+  console.log('Contract deployed to:', waveContract.address)
+
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address,
+  )
+  console.log(
+    'Contract Balance:',
+    hre.ethers.utils.formatEther(contractBalance),
+  )
+
+  let waveCount
+  waveCount = await waveContract.getTotalWaves()
+  console.log(waveCount.toNumber())
+
+  let waveTxn = await waveContract.wave('A New Message!')
+  await waveTxn.wait()
+
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address)
+  console.log('Contract Balance', hre.ethers.utils.formatEther(contractBalance))
+
+  const [_, randomPerson] = await hre.ethers.getSigners()
+  waveTxn = await waveContract.connect(randomPerson).wave('Another message!')
+  await waveTxn.wait()
+
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address)
+  console.log('Contract Balance', hre.ethers.utils.formatEther(contractBalance))
+
+  let allwaves = await waveContract.getAllWaves()
+  console.log(allwaves)
+}
+
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+runMain()
